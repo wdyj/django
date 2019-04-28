7bd1456b2db0700b2d3128bd9b16417f9edfb933
Lee-YeJin <poro@boozaapp.com>
2019-04-28 18:09:29 +0900

    동기간 계산 함수 추가

--- boozaapp/dashboard/views.py
+++ boozaapp/dashboard/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from dashboard.models import *
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
 
-# Create your views here.
+def calc_before_date(start, end) :
+    """
+    Args :
+        start, end => datetime
+    Returns :
+        b_start, b_end => datetime
+    """
+    
+    # interval 계산
+    if start.month > end.month :
+        period = (12+end.month) - start.month
+    else :
+        period = end.month - start.month
+    
+    #
+    if period in (0,1) :
+        period = 1
+    
+    # b_end가 start를 넘어설 경우 -> 한 달 더 빼기
+    if start.month != end.month and start.day < end.day :
+        period += 1
+    
+    b_start = start - relativedelta(months=period)
+    b_end = end - relativedelta(months=period)
+    
+#    if b_end > start :
+#        b_start = start - relativedelta(months=1)
+#        b_end = end - relativedelta(months=1)
+    
+    return b_start, b_end
